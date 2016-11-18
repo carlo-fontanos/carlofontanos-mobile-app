436e5f9a9e6df8dae9a54a312985251967ab8adf
Carlo Fontanos <carl.fontanos@gmail.com>
2016-11-18 16:37:47 +0800

    Hooked up the pages to the sidebar nav. Setup global variables and functions.

---
+++ MyApp.js
@@ -0,0 +1,22 @@
+/**
+ * Global Variables and Functions.
+ */
+ 
+var icons_path = 'assets/icon/';
+var app_pages = [
+	{'name':'HOME','ux':'homePage','icon':icons_path + 'page-home.png'},
+	{'name':'ABOUT','ux':'aboutPage','icon':icons_path + 'page-about.png'},
+	{'name':'TUTORIALS','ux':'tutorialsPage','icon':icons_path + 'page-tutorials.png'},
+	{'name':'PORTFOLIO','ux':'portfolioPage','icon':icons_path + 'page-portfolio.png'},
+	{'name':'PRODUCTS','ux':'productsPage','icon':icons_path + 'page-products.png'},
+	{'name':'CONTACT','ux':'contactPage','icon':icons_path + 'page-contact.png'}
+];
+
+function go_to_page(args){
+	router.push(args.data.ux);
+}
+
+module.exports = {
+	app_pages: app_pages,
+	go_to_page: go_to_page
+};
